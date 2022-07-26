ce0f38251e3de41e2b91776387187e1e3afb68ec
Federico Ghelfi <fedeghelfi9@gmail.com>
2022-07-26 18:29:13 +0200

    Settato il primo VC come TableViewController, embeddato in Navigation Controller. Creato AddViewController.

---
+++ MyCoupons/AddCouponViewController.h
@@ -0,0 +1,17 @@
+//
+//  AddCouponViewController.h
+//  MyCoupons
+//
+//  Created by Federico Ghelfi on 26/07/2022.
+//  Copyright © 2022 Federico Ghelfi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AddCouponViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyCoupons/AddCouponViewController.m
@@ -0,0 +1,32 @@
+//
+//  AddCouponViewController.m
+//  MyCoupons
+//
+//  Created by Federico Ghelfi on 26/07/2022.
+//  Copyright © 2022 Federico Ghelfi. All rights reserved.
+//
+
+#import "AddCouponViewController.h"
+
+@interface AddCouponViewController ()
+
+@end
+
+@implementation AddCouponViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
