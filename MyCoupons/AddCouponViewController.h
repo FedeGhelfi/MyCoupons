6b08505fcb6a589865a1753653cddc6837401ed2
Federico Ghelfi <fedeghelfi9@gmail.com>
2022-07-28 18:45:08 +0200

    Configurato AddCouponViewController con label e textfield. Impostati i delegate.

--- MyCoupons/AddCouponViewController.h
+++ MyCoupons/AddCouponViewController.h
@@ -10,7 +10,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface AddCouponViewController : UIViewController
+@interface AddCouponViewController : UIViewController<UITextFieldDelegate>
 
 @end
 
--- MyCoupons/AddCouponViewController.m
+++ MyCoupons/AddCouponViewController.m
@@ -10,6 +10,11 @@
 
 @interface AddCouponViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *CouponNameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *CompanyNameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *CodeTextField;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *ChoiceCodeFormat;
+
 @end
 
 @implementation AddCouponViewController
@@ -17,6 +22,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    
+    // il view controller diventa il delegate dei text field
+    self.CouponNameTextField.delegate = self;
+    self.CompanyNameTextField.delegate = self;
+    self.CodeTextField.delegate = self;
+
+}
+
+// chiamato appena prima che che il text field diventi attivo
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField{
+    NSLog(@"textFieldShouldBeginEditing");
+    textField.backgroundColor = [UIColor colorWithRed:220.0f/255.0f green:220.0f/255.0f blue:220.0f/255.0f alpha:0.3f];
+    return YES;
+}
+ 
+// chiamato quando diventa attivo il textfield
+- (void)textFieldDidBeginEditing:(UITextField *)textField{
+    NSLog(@"textFieldDidBeginEditing");
+}
+
+// Metodo che fa sparire la tastiera
+- (BOOL) textFieldShouldReturn:(UITextField *)textField{
+    NSLog(@"TextFieldShouldReturn");
+    
+    [textField resignFirstResponder];
+    return YES;
 }
 
 /*
